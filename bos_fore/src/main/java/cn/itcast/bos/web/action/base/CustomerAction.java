ceb6e183dcaa112f845df8b054d08a6c01f73f2f
gc <gc22331@hotmail.com>
2018-05-13 00:28:26 +0800

    first commit

---
+++ bos_fore/src/main/java/cn/itcast/bos/web/action/base/CustomerAction.java
@@ -0,0 +1,27 @@
+package cn.itcast.bos.web.action.base;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import cn.itcast.bos.web.action.common.BaseAction;
+import cn.itcast.crm.domain.Customer;
+
+@ParentPackage("struts-default")
+@Namespace("/")
+@Controller
+@Scope("prototype")
+public class CustomerAction extends BaseAction<Customer> {
+
+
+    @Action(value = "customer_regist", results = {
+            @Result(name = "success", type = "redirect", location = "./signup-success.html"),
+            @Result(name = "error", type = "redirect", location = "./signup.html")})
+    public String regist() {
+
+        return SUCCESS;
+    }
+}
---
+++ bos_management/src/main/java/cn/itcast/bos/web/action/base/ImageAction.java
@@ -0,0 +1,46 @@
+package cn.itcast.bos.web.action.base;
+
+import cn.itcast.bos.web.action.common.BaseAction;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@ParentPackage("json-default")
+@Namespace("/")
+@Controller
+@Scope("prototype")
+public class ImageAction extends BaseAction {
+
+    private String imgFile;
+    private String imgFileFileName;
+    private String imgFileContentType;
+
+
+    /**
+     * 实现文件上传到服务器
+     *
+     * @return 成功时返回url error = 0  失败时返回 error = 1 message
+     */
+    @Action(value = "image_upload", results = {@Result(type = "json")})
+    public String upload() {
+        System.out.println(imgFile);
+        System.out.println(imgFileFileName);
+        System.out.println(imgFileContentType);
+        return SUCCESS;
+    }
+
+    public void setImgFile(String imgFile) {
+        this.imgFile = imgFile;
+    }
+
+    public void setImgFileFileName(String imgFileFileName) {
+        this.imgFileFileName = imgFileFileName;
+    }
+
+    public void setImgFileContentType(String imgFileContentType) {
+        this.imgFileContentType = imgFileContentType;
+    }
+}
