f22a94f5c7665c9a9523de699a66ad7b90fc7925
gc <gc22331@hotmail.com>
2018-05-14 09:25:35 +0800

    上传宣传图片功能

--- bos_management/src/main/java/cn/itcast/bos/web/action/base/ImageAction.java
+++ bos_management/src/main/java/cn/itcast/bos/web/action/base/ImageAction.java
@@ -1,5 +1,9 @@
 package cn.itcast.bos.web.action.base;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
@@ -7,11 +11,19 @@ import org.apache.struts2.convention.annotation.Result;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ParentPackage("json-default")
 @Namespace("/")
 @Controller
 @Scope("prototype")
-public class ImageAction {
+public class ImageAction extends ActionSupport {
 
     private String imgFile;
     private String imgFileFileName;
@@ -28,7 +40,61 @@ public class ImageAction {
         System.out.println(imgFile);
         System.out.println(imgFileFileName);
         System.out.println(imgFileContentType);
-        return "success";
+        String savePath = ServletActionContext.getServletContext().getRealPath("/upload");
+        String urlPath = ServletActionContext.getServletContext().getContextPath() + "/upload/" + imgFileFileName;
+        System.out.println("图片访问路径:" + urlPath);
+        System.out.println("图片保存路径:" + savePath);
+        try {
+            FileUtils.copyFile(new File(imgFile), new File(savePath + "/" + imgFileFileName));
+            Map<String, Object> map = new HashMap<>();
+            map.put("error", 0);
+            map.put("url", urlPath);
+            ActionContext.getContext().getValueStack().push(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return SUCCESS;
+    }
+
+    @Action(value = "image_manager", results = {@Result(type = "json")})
+    public String picmanage() {
+        String realPath = ServletActionContext.getServletContext().getRealPath("/upload/");
+        File directory = new File(realPath);
+        List<Map<String, Object>> fileList = new ArrayList<>();
+        if (directory.listFiles() != null) {
+            for (File file : directory.listFiles()) {
+                Map<String, Object> hash = new HashMap<>();
+                String fileName = file.getName();
+                if (file.isDirectory()) {
+                    hash.put("is_dir", true);
+                    hash.put("has_file", (file.listFiles() != null));
+                    hash.put("filesize", 0L);
+                    hash.put("is_photo", false);
+                    hash.put("filetype", "");
+                } else {
+                    String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+                    hash.put("is_dir", false);
+                    hash.put("has_file", false);
+                    hash.put("filesize", file.length());
+                    hash.put("is_photo", true);
+                    hash.put("filetype", fileExt);
+                }
+                hash.put("filename", fileName);
+                hash.put("datetime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(file.lastModified()));
+                fileList.add(hash);
+            }
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("moveup_dir_path", "");
+        result.put("current_dir_path", realPath);
+        result.put("current_url", ServletActionContext.getRequest().getContextPath() + "/upload/");
+        result.put("total_count", fileList.size());
+        result.put("file_list", fileList);
+
+        ActionContext.getContext().getValueStack().push(result);
+
+        return SUCCESS;
     }
 
     public void setImgFile(String imgFile) {
